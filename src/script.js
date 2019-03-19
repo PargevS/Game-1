40a32f56e2bf466893513d56e72d82f5cb3132d5
Sargsyan Pargev <sargsyanpargev1991@gmail.com>
2019-03-19 19:11:46 +0100

    start and end of the game

--- src/script.js
+++ src/script.js
@@ -1,25 +1,64 @@
 const $start = document.querySelector('#start');
 const $game = document.querySelector('#game');
+const $time = document.querySelector('#time');
+const $result = document.querySelector('#result');
+const $timeHeader = document.querySelector('#time-header');
+const $resultHeader = document.querySelector('#result-header');
 
 $start.addEventListener('click', startGame);
-$game.addEventListener('click', handlBoxClick);
+
+
 
 let score = 0;
+let isGameStarted = false;
 
 // handlBoxClick
 function handlBoxClick(ev){
+  if(!isGameStarted ){
+      return;
+  }
   if(ev.target.dataset){
     score ++;
     renderBox();
+    ev.preventDefault();
   }
 }
 
 // start game 
 function startGame(ev){
-   $game.style.backgroundColor = '#fff';
-   $start.classList.add('hide');
+  isGameStarted = true;
+  $game.style.backgroundColor = '#fff';
+  $start.classList.add('hide');
+  let interval = setInterval( () => {
+  let time = parseFloat($time.textContent);
+
+    if(time <= 0){
+      clearInterval(interval);
+      endGame();
+    }else{
+      $time.textContent = (time - 0.1).toFixed(1);
+    }
+
+  },100);
+
    renderBox();
 } 
+
+//game score
+function setGameScore(){
+  $result.textContent = score.toString();
+}
+
+// end game
+function endGame(){
+  isGameStarted = false;
+  setGameScore();
+  $start.classList.remove('hide');
+  $game.innerHTML = '';
+  $game.style.backgroundColor = '#ccc';
+  $timeHeader.classList.add('hide');
+  $resultHeader.classList.remove('hide');
+}
 
 // boxes render
 function renderBox(){
@@ -37,14 +76,17 @@ function renderBox(){
   box.style.left = getRandom(0,maxLeft) + 'px';
   box.style.cursor = 'pointer';
   box.setAttribute('data-box','true');
+  box.addEventListener('click', handlBoxClick);
 
   $game.insertAdjacentElement('afterbegin',box);
 }
 
+//randomly changing the size and coordinates of the square
 function getRandom(min,max){
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+// Random color change
 function getRandomColor(){
   let index = Math.floor(Math.random() * 10);
   const colors = ['#FF0000','#FFC618','#FF188D','#A2FF18','#8E18FF','#18FF98','#FFA018','#FF4C18','#3FFF18','#000'];
