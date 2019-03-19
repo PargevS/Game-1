60b7c3822862bf84ea2213df6553901df205fe56
Sargsyan Pargev <sargsyanpargev1991@gmail.com>
2019-03-19 17:26:09 +0100

    functios renderBox and startGame

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,27 @@
-"use strict";
+'use strict';
+
+var $start = document.querySelector('#start');
+var $game = document.querySelector('#game');
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handlBoxClick);
+
+// start game 
+function startGame(ev) {
+  $game.style.backgroundColor = '#fff';
+  $start.classList.add('hide');
+  renderBox();
+}
+
+// boxes render
+function renderBox() {
+  var box = document.createElement('div');
+  box.style.height = box.style.width = '50px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = '50px';
+  box.style.left = '50px';
+  box.style.cursor = 'pointer';
+
+  $game.insertAdjacentElement('afterbegin', box);
+}
---
+++ src/script.js
@@ -0,0 +1,25 @@
+const $start = document.querySelector('#start');
+const $game = document.querySelector('#game');
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handlBoxClick);
+
+// start game 
+function startGame(ev){
+   $game.style.backgroundColor = '#fff';
+   $start.classList.add('hide');
+   renderBox();
+} 
+
+// boxes render
+function renderBox(){
+  let box = document.createElement('div');
+  box.style.height = box.style.width = '50px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#000';
+  box.style.top = '50px';
+  box.style.left = '50px';
+  box.style.cursor = 'pointer';
+
+  $game.insertAdjacentElement('afterbegin',box);
+}
